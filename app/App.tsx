3b9b5a72cd06b905c617e12e91d70dc362f897a4
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2025-01-08 15:24:38 +0300

    update 5 files and move 1 file: We are working now

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import './global.css';
+import './src/global.css';
 
 export default function App() {
     return (
--- app/babel.config.js
+++ app/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [
-            'module:metro-react-native-babel-preset',
+            // 'module:metro-react-native-babel-preset',
             ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
             'nativewind/babel',
         ],
